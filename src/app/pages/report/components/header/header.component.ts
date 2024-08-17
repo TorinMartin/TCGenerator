0aff8a4c2c9a1d54cd8dae7d0c40de7c6a066478
Torin Martin <alowsedan@gmail.com>
2024-08-17 19:14:46 -0300

    Layout

--- src/app/pages/report/components/header/header.component.ts
+++ src/app/pages/report/components/header/header.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'report-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  readonly typeOptions: string[] = ['TC', 'DITC'];
+  selectedType: string = this.typeOptions[1];
 
+  selectType(option: string) {
+    this.selectedType = option;
+  }
 }
--- src/app/pages/report/components/involved-persons/involved-persons.component.ts
+++ src/app/pages/report/components/involved-persons/involved-persons.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'report-involved-persons',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgbDropdownModule],
   templateUrl: './involved-persons.component.html',
   styleUrl: './involved-persons.component.css'
 })
 export class InvolvedPersonsComponent {
+  readonly codeOptions: string[] = ['D', 'P', 'W', 'PED'];
+  readonly sexOptions: string[] = ['M', 'F'];
+  readonly beltedOptions: string[] = ['Y', 'N'];
+  readonly statusOptions: string[] = ['UNHARMED', 'INJURED', 'SEVERELY INJURED', 'DECEASED'];
+  selectedCode: string = this.codeOptions[0];
+  selectedSex: string = this.sexOptions[0];
+  selectedBelted: string = this.beltedOptions[0];
+  selectedStatus: string = this.statusOptions[0];
 
+  selectCode(option: string) {
+    this.selectedCode = option;
+  }
+
+  selectSex(option: string) {
+    this.selectedSex = option;
+  }
+
+  selectBelted(option: string) {
+    this.selectedBelted = option;
+  }
+
+  selectStatus(option: string) {
+    this.selectedStatus = option;
+  }
 }
