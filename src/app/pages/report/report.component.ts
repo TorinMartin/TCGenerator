90d5d70667e14ad14cc744901fc3bd50d232d12c
TorinMartin <alowsedan@gmail.com>
2024-08-19 18:04:36 -0300

    Copy report to clipboard

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -18,13 +18,37 @@ export class ReportComponent {
 
   generate() {
     this.isLoading = true;
-    this.bbCodeContent = '';
-    this.bbCodeSubject = '';
+    this.bbCodeContent = "";
+    this.bbCodeSubject = "";
     setTimeout(() => {
       this.bbCodeSubject = this._generationService.generateTitle();
       this.bbCodeContent = this._generationService.generate();
       this.toastr.success('Your report has been generated!', 'Success');
       this.isLoading = false;
     }, 200);
+  }
+
+  copyTitle() {
+    if (this.bbCodeSubject === "") {
+      this.toastr.error('Generate a report first.', 'Nothing To Copy');
+    }
+    else {
+      navigator.clipboard.writeText(this.bbCodeSubject).then(
+        () => this.toastr.info('Report subject copied.', 'Copied Subject'),
+        () => this.toastr.info('Unable to copy report subject!', 'Error')
+      );
+    }
+  }
+
+  copyBody() {
+    if (this.bbCodeContent === "") {
+      this.toastr.error('Generate a report first.', 'Nothing To Copy');
+    }
+    else {
+      navigator.clipboard.writeText(this.bbCodeContent).then(
+        () => this.toastr.info('Report BBCode copied.', 'Copied BBCode'),
+        () => this.toastr.info('Unable to copy report content', 'Error')
+      );
+    }
   }
 }
